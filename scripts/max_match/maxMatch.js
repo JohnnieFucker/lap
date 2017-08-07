7b55b46930e38d410ce6050d5913b1e5c77be2f8
ShineLee <shine@vip.qq.com>
2017-08-07 17:27:57 +0800

    实现最大匹配分词法

--- scripts/max_match/maxMatch.js
+++ scripts/max_match/maxMatch.js
@@ -29,7 +29,7 @@ function testSentence(sentence,max,direction){
         return;
     }
     let sigleWord = testWords(subSentence,direction);
-    let sigleWordNoSymbol = sigleWord.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,"");
+    let sigleWordNoSymbol = sigleWord.replace(/[\n|\r|\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,"");
     sigleWordNoSymbol = sigleWordNoSymbol.replace(/[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/,'');
     if(direction==='asc'){
         if(sigleWordNoSymbol !==''){
